76b0c1a92af6412560161a6a4eac29abee9358b1
Anishjindal22 <anish.jindal1983@gmail.com>
2024-05-14 18:57:06 +0530

    project added

---
+++ feeproj.js
@@ -0,0 +1,25 @@
+var button = document.getElementById("redirectButton");
+button.addEventListener("click", function () {
+  var redirectToUrl = "https://en.wikipedia.org/wiki/MS_Dhoni";
+  window.location.href = redirectToUrl;
+});
+var button = document.getElementById("inst");
+button.addEventListener("click", function () {
+  var redirectToUrl = "//";
+  window.location.href = redirectToUrl;
+});
+var button = document.getElementById("face");
+button.addEventListener("click", function () {
+  var redirectToUrl = "//";
+  window.location.href = redirectToUrl;
+});
+var button = document.getElementById("twitt");
+button.addEventListener("click", function () {
+  var redirectToUrl = "//";
+  window.location.href = redirectToUrl;
+});
+var button = document.getElementById("thread");
+button.addEventListener("click", function () {
+  var redirectToUrl = "///";
+  window.location.href = redirectToUrl;
+});
